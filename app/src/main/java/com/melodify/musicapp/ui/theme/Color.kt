06b47f6c1d4fe63aa3f5018dc922b71748264658
ashkan moshfegh <moshfeghashkan@gmail.com>
2026-07-20 19:02:28 +0330

    heab1

--- app/src/main/java/com/melodify/musicapp/ui/theme/Color.kt
+++ app/src/main/java/com/melodify/musicapp/ui/theme/Color.kt
@@ -2,22 +2,19 @@ package com.melodify.musicapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Global Red Theme
 val Red80 = Color(0xFFFFB4AB)
 val RedGrey80 = Color(0xFFD8C2BF)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Red40 = Color(0xFFBA1A1A)
 val RedGrey40 = Color(0xFF93000A)
-val Pink40 = Color(0xFF7D5260)
 
-// Melodify Custom Colors - Red Theme
-val PrimaryLight = Color(0xFFBA1A1A)
-val PrimaryVariantLight = Color(0xFF93000A)
-val SecondaryLight = Color(0xFF775652)
+val PrimaryLight = Color(0xFFD32F2F)
+val PrimaryVariantLight = Color(0xFFB71C1C)
+val SecondaryLight = Color(0xFFF44336)
 
-val PrimaryDark = Color(0xFFFFB4AB)
-val PrimaryVariantDark = Color(0xFF93000A)
-val SecondaryDark = Color(0xFFE7BDB7)
+val PrimaryDark = Color(0xFFFF8A80)
+val PrimaryVariantDark = Color(0xFFD32F2F)
+val SecondaryDark = Color(0xFFFF5252)
 
 val PremiumGold = Color(0xFFFFD700)
 val ShimmerBase = Color(0xFFE0E0E0)
