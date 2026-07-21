be9df038e2d736eccf61749ee6d1a78aafe117eb
ashkan moshfegh <moshfeghashkan@gmail.com>
2026-07-21 16:26:17 +0330

    merged and built

--- app/src/main/java/com/example/melodify/musicapp/data/local/entity/PlaylistSongEntity.kt
+++ app/src/main/java/com/example/melodify/musicapp/data/local/entity/PlaylistSongEntity.kt
@@ -1,10 +1,21 @@
 package com.melodify.musicapp.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 
 @Entity(
     tableName = "playlist_songs",
-    primaryKeys = ["playlistId", "songId"]
+    primaryKeys = ["playlistId", "songId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = SongEntity::class,
+            parentColumns = ["songId"],
+            childColumns = ["songId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("songId")]
 )
 data class PlaylistSongEntity(
     val playlistId: String,
